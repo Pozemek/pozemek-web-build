a29ff0cb6f4971df788a211a73bdf053209b8962
ShazibCheema <mrshazib7@gmail.com>
2025-12-12 16:14:02 +0500

    update from client feedback v1.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "58907976"
+    serviceWorkerVersion: "391714521"
   }
 });
