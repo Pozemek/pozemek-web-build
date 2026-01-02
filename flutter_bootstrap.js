792873b84e6f6e5cf9169dee0c1f6012e849c256
ShazibCheema <mrshazib7@gmail.com>
2026-01-02 22:57:59 +0500

    payment flow testingv1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4291890169"
+    serviceWorkerVersion: "2153917286"
   }
 });
