2b10b46b733df3c220aacd0356af1b3af4b6d15d
ShazibCheema <mrshazib7@gmail.com>
2026-01-03 18:19:20 +0500

    listing detail updated!

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "971344506"
+    serviceWorkerVersion: "2426446382"
   }
 });
